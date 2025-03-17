d999956a4e66a29edc2d75ec057bf2de2b6514d4
NuVega <47011763+NuVega@users.noreply.github.com>
2025-03-17 19:28:05 +0300

    csv

--- divan_parser/divan_parser/spiders/divan_spider.py
+++ divan_parser/divan_parser/spiders/divan_spider.py
@@ -1,38 +1,70 @@
 import scrapy
+import csv
+import os
+
 
 class DivanLightingSpider(scrapy.Spider):
     name = "divan_lighting"
     allowed_domains = ["divan.ru"]
-    start_urls = ["https://www.divan.ru/category/svet"]  # Категория освещения
+    start_urls = ["https://www.divan.ru/category/svet"]
+    item_counter = 1  # Счётчик товаров
+
+    def __init__(self, *args, **kwargs):
+        super(DivanLightingSpider, self).__init__(*args, **kwargs)
+        filename = "divan_lighting.csv"
+        if os.path.exists(filename):
+            os.remove(filename)
+            self.logger.info(f"Удален существующий файл {filename}")
 
     def parse(self, response):
-        # Находим карточки товаров
+        items = []
         products = response.css("div[data-testid='product-card']")
 
         for product in products:
-            yield {
-                "name": product.css("a.ProductName span[itemprop='name']::text").get(),
-                "price": product.css("div.pY3d2 span[data-testid='price']::text").get(),
-                "link": response.urljoin(product.css("a.ProductName::attr(href)").get())
+            item = {
+                "№": self.item_counter,
+                "Наименование": product.css("a.ProductName span[itemprop='name']::text").get(),
+                "Цена": product.css("div.pY3d2 span[data-testid='price']::text").get(),
+                "Ссылка": response.urljoin(product.css("a.ProductName::attr(href)").get())
             }
+            self.item_counter += 1
+            items.append(item)
+            yield item  # отдаём в Scrapy пайплайны, если нужны
 
-        # ПАГИНАЦИЯ
-        
-        # Получаем все ссылки на страницы
+        if items:
+            self.save_to_csv(items)
+
+        # Пагинация
         pages = response.css("a.PaginationLink::attr(href)").getall()
+        if not pages:
+            self.logger.info("Страницы пагинации не найдены")
+            return
 
-        # Убираем дубликаты (сохраняя порядок)
-        pages = list(dict.fromkeys(pages))
-
-        # Определяем текущую страницу
+        pages = list(dict.fromkeys(pages))  # Убираем дубликаты
         current_page_index = None
         for i, page in enumerate(pages):
-            if response.url.endswith(page):  # Текущая страница
+            if response.url.endswith(page):
                 current_page_index = i
                 break
 
-        # Если есть следующая страница
         if current_page_index is not None and current_page_index + 1 < len(pages):
             next_page = pages[current_page_index + 1]
             yield response.follow(next_page, callback=self.parse)
+        else:
+            self.logger.info("Достигнута последняя страница или не удалось определить текущую страницу")
 
+    def save_to_csv(self, items):
+        filename = "divan_lighting.csv"
+        fieldnames = ["№", "Наименование", "Цена", "Ссылка"]
+
+        file_exists = os.path.exists(filename)
+
+        with open(filename, "a", newline="", encoding="utf-8") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            if not file_exists:
+                writer.writeheader()
+            writer.writerows(items)
+
+    def closed(self, reason):
+        self.logger.info(f"Паук завершил работу по причине: {reason}")
+        self.logger.info(f"Всего собрано товаров: {self.item_counter - 1}")
