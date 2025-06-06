260b177a338db6355eca5435e0b9b9b2bb92ddf9
NuVega <47011763+NuVega@users.noreply.github.com>
2025-06-06 14:26:29 +0300

    weather-bot-emoji
    Добавил разные эмодзи через main

--- 11_TG/TG01_aio/main.py
+++ 11_TG/TG01_aio/main.py
@@ -7,11 +7,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+
 bot = telebot.TeleBot(BOT_TOKEN)
+
+weather_icons = {
+    "clear": "☀️",
+    "clouds": "☁️",
+    "rain": "🌧",
+    "drizzle": "🌦",
+    "thunderstorm": "⛈",
+    "snow": "❄️",
+    "mist": "🌫",
+    "fog": "🌫"
+}
 
 @bot.message_handler(commands=['start'])
 def start(message: Message):
-    bot.send_message(message.chat.id, "Привет! Я покажу тебе погоду. Напиши /weather")
+    bot.send_message(message.chat.id, "Привет! Я покажу тебе погоду в Петербурге — просто используй команду /weather.\n\nЕсли хочешь узнать погоду в другой локации — пропиши вручную /weather и название города на английском (пример: /weather Tokyo)")
 
 @bot.message_handler(commands=['help'])
 def help_command(message: Message):
@@ -19,25 +32,26 @@ def help_command(message: Message):
 
 @bot.message_handler(commands=['weather'])
 def weather(message: Message):
-    import os
-    city = "Saint Petersburg"
-    lat, lon = 59.93, 30.31
-    API_KEY = os.getenv("WEATHER_API_KEY")
+    parts = message.text.strip().split(maxsplit=1)
+    city = parts[1] if len(parts) > 1 else "Saint Petersburg"
 
-    if not API_KEY:
-        bot.send_message(message.chat.id, "API-ключ не найден в переменных окружения.")
-        return
-
-    url = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units=metric&lang=ru&appid={API_KEY}"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=ru&appid={WEATHER_API_KEY}"
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        temp = data['current']['temp']
-        desc = data['current']['weather'][0]['description']
-        bot.send_message(message.chat.id, f"🌤 Погода в {city}:\nТемпература: {temp}°C\nСостояние: {desc}")
+        temp = data['main']['temp']
+        desc = data['weather'][0]['description']
+        main = data['weather'][0]['main']
+        icon = weather_icons.get(main.lower(), "🌡")  # fallback на универсальный значок
+        bot.send_message(message.chat.id, f"{icon} Погода в {city}:\nТемпература: {temp}°C\nСостояние: {desc}")
+    elif response.status_code == 404:
+        bot.send_message(message.chat.id, f"Город '{city}' не найден.")
     else:
         bot.send_message(message.chat.id, "Ошибка при получении погоды.")
+        print("Ошибка:", response.status_code)
+        print(response.text)
+
 
 
 if __name__ == "__main__":
